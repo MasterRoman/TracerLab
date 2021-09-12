e234607f00239f7fd9df190607013ac4c65eb1da
Roman Kozko <romanka2001@mail.ru>
2021-09-12 23:49:08 +0300

    implement thread tracer

---
+++ TracerLab/TracerLibrary/ThreadTracer.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TracerLibrary
+{
+    class ThreadTracer
+    {
+
+        private ThreadResult curInfo;
+        private Stack<MethodTracer> callStack;
+
+        public ThreadTracer(int id)
+        {
+            this.curInfo = new ThreadResult(id);
+            this.callStack = new Stack<MethodTracer>();
+        }
+
+        public void StartTrace()
+        { 
+            var newMethodTracer = new MethodTracer();
+            callStack.Push(newMethodTracer);
+
+            newMethodTracer.StartTrace();
+        }
+
+        public void StopTrace()
+        {
+            var lastTracer = callStack.Pop();
+            lastTracer.StopTrace();
+
+            var method = lastTracer.GetTraceResult();
+            curInfo.addTime(method.time);
+
+            if (callStack.Count > 0)
+            {
+                var newLastTracer = callStack.Peek();
+                newLastTracer.addChildResult(method);
+            } else
+            {
+                curInfo.addMethod(method);
+            }
+
+        }
+
+        public ThreadResult GetTraceResult()
+        {
+            return curInfo;
+        }
+
+    }
+}
