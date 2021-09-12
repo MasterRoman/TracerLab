364ce236e695f6ddda184ec8affcc9b622adc968
Roman Kozko <romanka2001@mail.ru>
2021-09-12 21:40:18 +0300

    add models for threads and methods

---
+++ TracerLab/TracerLibrary/MethodResult.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TracerLibrary
+{
+    struct MethodResult
+    {
+        public string name { get; }
+        public string className { get; }
+
+        public double time { get; private set; }
+
+        public List<MethodResult> childMethodsResult { get; private set; }
+    }
+}
---
+++ TracerLab/TracerLibrary/ThreadResult.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TracerLibrary
+{
+    struct ThreadResult
+    {
+        public int id { get; }
+        public double time { get; private set; }
+
+        public List<MethodResult> methodsResult { get; private set; }
+
+    }
+}
