d42a1537664e1bac16edf26d3baa76edf30e3c5e
Roman Kozko <romanka2001@mail.ru>
2021-09-13 13:56:20 +0300

    implement xml serializer

--- TracerLab/TracerLibrary/XMLSerializer.cs
+++ TracerLab/TracerLibrary/XMLSerializer.cs
@@ -2,14 +2,71 @@
 using System.Collections.Generic;
 using System.Text;
 
+using System.Xml.Linq;
+using System.IO;
+using System.Xml;
+
 namespace TracerLibrary
 {
-    class XMLSerializer : ISerializer
+    public class XMLSerializer : ISerializer
     {
         public string Serialize(TraceResult result)
         {
-            return "";
+            var threadsResults = result.threadResults;
+
+            XDocument xDoc = new XDocument(new XElement("root"));
+
+            foreach (ThreadResult threadResult in threadsResults)
+            {
+                var threadXElement = configureThreadXElement(threadResult);
+                xDoc.Root.Add(threadXElement);
+            }
+
+            StringWriter stringWriter = new StringWriter();
+            using (XmlTextWriter xmlWriter = new XmlTextWriter(stringWriter))
+            {
+              //  xmlWriter.Formatting = Formatting.Indented;
+                xDoc.WriteTo(xmlWriter);
+            }
+            return stringWriter.ToString();
         }
 
+        private XElement configureThreadXElement(ThreadResult threadResult)
+        {
+            XElement threadXElement = new XElement("thread", new XAttribute("id", threadResult.id),
+                                                            new XAttribute("time", threadResult.time)
+                                                   );
+            foreach (MethodResult methodResult in threadResult.methodsResult)
+            {
+                XElement methodXElement = configureMethodXElement(methodResult);
+                threadXElement.Add(methodXElement);
+            }
+            return threadXElement;
+        }
+
+        private XElement configureMethodXElement(MethodResult methodResult)
+        {
+            XElement methodXElement = new XElement("method", new XAttribute("name", methodResult.name),
+                                                            new XAttribute("className", methodResult.className),
+                                                            new XAttribute("time", methodResult.time)
+                                                   );
+            foreach (MethodResult childMethodResult in methodResult.childMethodsResult)
+            {
+                if (childMethodResult.childMethodsResult.Count > 0)
+                {
+                    XElement childMethodXElement = configureMethodXElement(childMethodResult);
+                    methodXElement.Add(childMethodXElement);
+                }
+            }
+            return methodXElement;
+        }
+
+
     }
-}
+
+}  
+
+       
+    
+
+
